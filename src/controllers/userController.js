70234037d7dbd835b5b114d8521e06c57e78bf22
sonhunter <117790523+mittoson@users.noreply.github.com>
2023-07-21 09:23:49 +0700

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,18 +6,32 @@ export const getUser = (req,res)=>{
     const data = getAll('users');
     return data;
 }
+let hashPassword =  (password)=>{
+    return bcrypt.hash(password,10,(err,hash)=>{
+        if(!err){
+            console.log(hash);
+            return hash
+        }else{
+            
+            console.log(err)
+            return null;
+        }
+    })
+}
 export const createUser = async (req, res)=>{
+const pass = hashPassword(req.body.password)
     let data ={
-        // 'user_name':req.body.user_name,
-        // 'password' : hashPassword,
-        // 'email' : req.body.email,
-        // 'phone' : req.body.phone,
-        // 'permission_id':req.body.permission_id
-        name : req.body.name,
-        description:req.body.description
+        'user_name':req.body.user_name,
+        'password' : pass,
+        'email' : req.body.email,
+        'phone' : req.body.phone,
+        'permission_id':req.body.permission_id
+        // name : req.body.name,
+        // description:req.body.description
     }
+    console.log(data);
     try {
-        const User = await createData(data);
+        const User = await createData('users',data);
         return res.status(201).json({
             message:"Created User success",
             data:User
@@ -31,16 +45,7 @@ export const createUser = async (req, res)=>{
     })
 }
 
-let hashPassword =  (req,res)=>{
-    bcrypt.hash(req.body.password,10,(err,hash)=>{
-        if(!err){
-            console.log(hash);
-            return hash
-        }else{
-            console.log(err)
-        }
-    })
-}
+
 // let re = bcrypt.compare('123456','$2b$10$AnUJmBWPASfBziR/u8/v7u/JVPnUNWNqtDrWYZG35J0I9SRAQdeDK',(err,result)=>{
 //     if(!err){
 //         console.log('oke')
